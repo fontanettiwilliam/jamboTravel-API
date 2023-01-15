d1646b21dd7b91cab677f58cd0ed64f05ed4843b
William Fontanetti <williampriester@gmail.com>
2023-01-15 09:07:37 -0700

    Fix - Server.ts, config router

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import express from "express";
 
+import { cityRouter } from "./modules/city/cityRouter.js";
+
 const server = express();
+
 server.use(express.json());
+
+server.use("/", cityRouter);
 
 server.listen(3333, () => {
   console.log("Server launched on port 3333!🚀");
