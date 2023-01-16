34fa7cc377cf1aaf705d049fba94786687dfbfc6
William Fontanetti <williampriester@gmail.com>
2023-01-15 20:20:17 -0700

    Deploy

--- dist/src/server.js
+++ dist/src/server.js
@@ -4,8 +4,24 @@ import express from "express";
 import { cityRouter } from "./modules/city/cityRouter.js";
 dotenv.config();
 const server = express();
-server.options("*", cors({ origin: process.env.WEB_URL, optionsSuccessStatus: 200 }));
-server.use(cors({ origin: process.env.WEB_URL, optionsSuccessStatus: 200 }));
+server.options("*", cors({
+    origin: "https://jambo-travel.vercel.app/",
+    optionsSuccessStatus: 200,
+}));
+server.use(cors({
+    origin: "https://jambo-travel.vercel.app/",
+    optionsSuccessStatus: 200,
+}));
+server.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader("Access-Control-Allow-Origin", "https://jambo-travel.vercel.app/");
+    // Request methods you wish to allow
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+    // Request headers you wish to allow
+    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type");
+    // Pass to next layer of middleware
+    next();
+});
 server.use(express.json());
 server.use("/", cityRouter);
 server.listen(3333, () => {
